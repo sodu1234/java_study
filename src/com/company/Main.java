b60efb2bf6273452db4d605f3bcc877f26188ab1
robinhur <sgrobinhur@gmail.com>
2018-09-15 15:56:40 +0900

    java modified

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
