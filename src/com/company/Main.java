03ba7b29e4cdb7599a103b92500ccac107ea6255
sodu1234 <35587272+sodu1234@users.noreply.github.com>
2018-10-05 06:51:38 +0900

    별찍기 12,13,15,16,20 성공 ㅠㅠ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,57 @@
 package com.company;
-////별찍기1번
+
+//별찍기16번
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1; i <= n ; i++){
+            for(int j=n-i; j > 0; j--){
+                System.out.print(" ");
+             }
+            for(int j=0; j< i*2-1; j++){
+                if( j % 2 ==0 || j==i*2-2)   {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+//   if(j==0 || j==i*2-2) {
+//   if(i%2==1){
+
+
+//별찍기1번
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for (int i = 0; i < n; ++i ){
+//            for (int j = 0; j < i+1 ; ++j ) {
+//                System.out.print("*");
+//            }
+//
+//
+//            System.out.println();
+//        }
+//    }
+//}
+
+//별찍기1번
 //import java.util.Scanner;
 //
 //public class Main {
@@ -220,3 +272,139 @@ package com.company;
 //    }
 //}
 
+
+//별찍기12
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for (int i = 0; i < n; ++i) {
+//            for (int j = 0; j < n - i - 1; ++j) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 0; k < i + 1; ++k) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//
+//        for (int i = 1; i < n; ++i) {
+//            for (int j = 0; j < i ; ++j) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 0; k < n-i; ++k) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//}
+
+
+
+//별찍기13
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for (int i = 0; i < n; ++i) {
+//            for (int j = 0; j < i + 1; ++j) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//            }
+//        for (int i = 1; i < n; ++i) {
+//            for (int j = 0; j < n - i; ++j) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+
+//별찍기15번
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for(int i=1; i <= n ; i++){
+//            for(int j=n-i; j > 0; j--){
+//                System.out.print(" ");
+//             }
+//            for(int j=0; j< i*2-1; j++){
+//                if(j==0 || j==i*2-2) {
+//                    System.out.print("*");
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+//
+
+//별찍기20번
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for(int i = 0; i< n; i++){
+//            if(i%2==1){
+//                System.out.print(" ");
+//            }
+//            for(int j=0; j< n; j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+//별찍기16번
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for(int i=1; i <= n ; i++){
+//            for(int j=n-i; j > 0; j--){
+//                System.out.print(" ");
+//            }
+//            for(int j=0; j< i*2-1; j++){
+//                if( j % 2 ==0 || j==i*2-2)   {
+//                    System.out.print("*");
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
