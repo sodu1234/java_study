af4513d4ed53fcde5d60e17c6783ebc554828f31
Administrator <babara001>
2018-10-14 10:07:10 +0900

    별찍기12,13 자가진단1,2,3 업데이트

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
-//별찍기16번
+//별찍기15번
 import java.util.Scanner;
 
 public class Main {
@@ -15,7 +15,7 @@ public class Main {
                 System.out.print(" ");
              }
             for(int j=0; j< i*2-1; j++){
-                if( j % 2 ==0 || j==i*2-2)   {
+                if(j==0 || j==i*2-2) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
@@ -26,8 +26,30 @@ public class Main {
     }
 }
 
-//   if(j==0 || j==i*2-2) {
-//   if(i%2==1){
+//import java.util.Scanner;
+//
+//class Printclass {
+//    void print(int n) {
+//        for (int i = n; i > 0; i--) {
+//            System.out.print(i);
+//            System.out.print(" ");
+//
+//        }
+//        System.out.println();
+//    }
+//}
+//
+//
+//public class Main {
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        new Printclass().print(n);
+//    }
+//
+//}
+
 
 
 //별찍기1번
@@ -202,7 +224,7 @@ public class Main {
 //    }
 //}
 
-//별찍기8번
+//별찍기 8번
 //import java.util.Scanner;
 //public class Main {
 //    public static void main(String[] args) {
@@ -238,7 +260,7 @@ public class Main {
 //    }
 //}
 
-// 별찍기9번
+// 별찍기 9번
 //import java.util.Scanner;
 //
 //public class Main {
@@ -273,7 +295,7 @@ public class Main {
 //}
 
 
-//별찍기12
+//별찍기 12_old
 //import java.util.Scanner;
 //
 //public class Main {
@@ -306,9 +328,29 @@ public class Main {
 //
 //}
 
-
-
-//별찍기13
+//별찍기 12
+//import java.util.Scanner;
+//        import static java.lang.Math.abs;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for(int i=0; i < n *2; i++){
+//            for(int j=0 ;j< abs(i - 2); j++) {
+//                System.out.print(" ");
+//            }
+//            for(int k=0 ; k < n-abs(i-2); k++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+//별찍기 13_old
 //import java.util.Scanner;
 //
 //public class Main {
@@ -333,8 +375,27 @@ public class Main {
 //    }
 //}
 
+//별찍기 13번
+//import java.util.Scanner;
+//        import static java.lang.Math.abs;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for(int i=0; i < n *2; i++){
+//            for(int j=0 ;j< n-abs(i - 2); j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
 
-//별찍기15번
+//별찍기 15번
 //import java.util.Scanner;
 //
 //public class Main {
@@ -351,9 +412,10 @@ public class Main {
 //            for(int j=0; j< i*2-1; j++){
 //                if(j==0 || j==i*2-2) {
 //                    System.out.print("*");
-//                } else {
-//                    System.out.print(" ");
 //                }
+//                    else {
+//                    System.out.print(" ");
+//                    }
 //            }
 //            System.out.println();
 //        }
@@ -361,29 +423,8 @@ public class Main {
 //}
 //
 
-//별찍기20번
-//import java.util.Scanner;
-//
-//public class Main {
-//
-//    public static void main(String[] args) {
-//
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
-//
-//        for(int i = 0; i< n; i++){
-//            if(i%2==1){
-//                System.out.print(" ");
-//            }
-//            for(int j=0; j< n; j++){
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//        }
-//    }
-//}
 
-//별찍기16번
+//별찍기 16번_old
 //import java.util.Scanner;
 //
 //public class Main {
@@ -410,8 +451,29 @@ public class Main {
 //}
 
 
+//별찍기 20번_old
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for(int i = 0; i< n; i++){
+//            if(i%2==1) {
+//                System.out.print(" ");
+//            }
+//            for(int j=0; j< n; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
 
-//자가진단1
+//자가진단1_old
 //import java.util.Scanner;
 //
 //class Printclass {
@@ -433,11 +495,33 @@ public class Main {
 //    }
 //
 //}
+
+//자가진단1
+//import java.util.Scanner;
 //
+//public class Main {
 //
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        print(n);
+//    }
+//
+//    static void print(int x) {
+//        if( x > 0 ){
+//            System.out.println("recursive");
+//            x--;
+//            print(x);
+//        }
+//
+//    }
+//}
 
 
-//자가진단2
+
+
+//자가진단2_old
 //import java.util.Scanner;
 //
 //class Printclass {
@@ -463,5 +547,48 @@ public class Main {
 //}
 
 
-//자가진단3
+//자가진단2
+//import java.util.Scanner;
 //
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        print(n);
+//    }
+//
+//    static void print(int x) {
+//        if( x > 0 ){
+//            System.out.print(x);
+//            System.out.print(" ");
+//
+//            x--;
+//            print(x);
+//        }
+//
+//    }
+//}
+
+//자가진단3
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int z= sum(n);
+//        System.out.println(z);
+//    }
+//
+//    static int sum(int x) {
+//
+//        if (x > 0) {
+//            return x + sum(x - 1);
+//        }
+//        return 0;
+//    }
+//
+//}
